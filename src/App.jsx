b1d5bde1f30d850d50652cdbe9279ef06e4b3b5d
CanaanL19 <canaanlock112@gmail.com>
2023-08-18 22:39:29 -0500

    Search Bar visual

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {SearchBar} from "./Components/SearchBar/SearchBar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 id="title">Jammming</h1>
+      <header className="App-header">
+      <div className="SearchBox">
+        <SearchBar />
+      </div>
+      </header>
+      
+  </div>
+  );
+}
+
+export default App;
